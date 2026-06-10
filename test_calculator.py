66fb3711d2b05d320f899db3458df8b720f65a9a
Elango <elango200327@gmail.com>
2026-06-10 14:13:46 +0530

    actions setup

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+from calculator import add, subtract,multiply, divide
+
+def test_add():
+    assert add(5, 3) == 8
+
+def test_subtract():
+    assert subtract(10, 4) == 6
+
+def test_multiply():
+    assert multiply(6, 7) == 42
+
+def test_divide():
+    assert divide(20, 5) == 4
